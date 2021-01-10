eb7561ccb6a1c79d24ede01ff0faf2d78b25098e
bloodsoul <2312178690@qq.com>
2021-01-11 07:38:25 +0800

    commit

--- app/src/main/java/com/blood/jetpackpaging3/paging/Paging3Activity.kt
+++ app/src/main/java/com/blood/jetpackpaging3/paging/Paging3Activity.kt
@@ -1,11 +1,13 @@
 package com.blood.jetpackpaging3.paging
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
+import androidx.paging.LoadState
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.blood.jetpackpaging3.R
 import com.blood.jetpackpaging3.databinding.ActivityPaging3Binding
@@ -24,12 +26,29 @@ class Paging3Activity : AppCompatActivity() {
         binding.rv.layoutManager = LinearLayoutManager(this)
         binding.rv.adapter = adapter
 
+        adapter.addLoadStateListener {
+            when(it.refresh) {
+                is LoadState.Loading -> log("refresh Loading")
+                is LoadState.NotLoading -> log("refresh NotLoading")
+                is LoadState.Error -> log("refresh Error")
+            }
+            when(it.append) {
+                is LoadState.Loading -> log("append Loading")
+                is LoadState.NotLoading -> log("append NotLoading")
+                is LoadState.Error -> log("append Error")
+            }
+        }
+
         val viewModel = ViewModelProvider(this).get(PagingViewModel::class.java)
         viewModel.articleList.observe(this, {
             lifecycleScope.launchWhenCreated {
                 adapter.submitData(it)
             }
         })
+    }
+
+    private fun log(msg : String) {
+        Log.d("Paging3", "log: $msg")
     }
 
 }
--- app/src/main/java/com/blood/jetpackpaging3/paging/PagingViewModel.kt
+++ app/src/main/java/com/blood/jetpackpaging3/paging/PagingViewModel.kt
@@ -1,12 +1,10 @@
 package com.blood.jetpackpaging3.paging
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
-import androidx.paging.Pager
-import androidx.paging.PagingConfig
-import androidx.paging.PagingSource
-import androidx.paging.cachedIn
+import androidx.paging.*
 
 
 /*
@@ -21,20 +19,43 @@ class PagingViewModel : ViewModel() {
 
     val netApi = NetApi.create()
 
-    val articleList = Pager(PagingConfig(pageSize = 30, prefetchDistance = 5), initialKey = 0) {
-        object : PagingSource<Int, DataX>() {
-            override suspend fun load(params: LoadParams<Int>): LoadResult<Int, DataX> {
-                val pagerNum = params.key ?: 0
-                return try {
-                    val result = netApi.load(pagerNum)
-                    LoadResult.Page(result.data.datas, null, pagerNum + 1)
-                } catch (e: Exception) {
-                    e.printStackTrace()
-                    LoadResult.Error(e)
+    @OptIn(ExperimentalPagingApi::class)
+    val articleList = Pager(
+        PagingConfig(
+            pageSize = 30,
+            prefetchDistance = 5,
+            enablePlaceholders = true,
+            initialLoadSize = 30
+        ),
+        initialKey = 0,
+        remoteMediator = object : RemoteMediator<Int, DataX>() {
+            override suspend fun load(loadType: LoadType, state: PagingState<Int, DataX>): MediatorResult {
+                Log.d("Paging3", "remoteMediator load: $loadType $state")
+                return MediatorResult.Success(false)
+            }
+        },
+        pagingSourceFactory = {
+            object : PagingSource<Int, DataX>() {
+                override suspend fun load(params: LoadParams<Int>): LoadResult<Int, DataX> {
+                    return try {
+                        val pagerNum = params.key ?: 0
+                        val result = netApi.load(pagerNum)
+                        val prevKey = if (pagerNum <= 0) null else pagerNum - 1
+                        val expectNextKey = pagerNum + 1
+                        val nextKey = if (expectNextKey > 3) null else expectNextKey
+                        Log.d("Paging3", "PagingSource load: $pagerNum")
+                        LoadResult.Page(
+                            result.data.datas,
+                            prevKey,
+                            nextKey
+                        )
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                        LoadResult.Error(e)
+                    }
                 }
             }
-        }
-    }
+        })
         .flow
         .cachedIn(viewModelScope)
         .asLiveData(viewModelScope.coroutineContext)
